fec376713c0257864e503782e2d0afac648b2f55
David Flores <ie717807@iteso.mx>
2019-02-26 01:41:46 -0600

    Building Initial Tower  and  Base Case

--- 09_P1_HanoiTowers_Recursivo.asm
+++ 09_P1_HanoiTowers_Recursivo.asm
@@ -6,36 +6,85 @@
 #		forma RECURSIVA"		#
 #///////////////////////////////////////////////#
 .data 
-torreA: .word 0
-
+torreA: .word 0 0 0 0 0 0 0 0	# Caso máx. con 8 discos
+torreB: .word 0 0 0 0 0 0 0 0
+torreC: .word 0 0 0 0 0 0 0 0
 
 .text
-##################	MAIN	 #################
+##################     MAIN     #################
 MAIN:
- addi $s0, $zero, 3	# En $s0 se guarda el NUM de DISCOS de la torre
+ addi $s0, $zero, 7	# En $s0 se guarda el NUM de DISCOS de la torre
  add $a0, $zero, $s0	# Parametro de DISCOS
- add $t0, $zero, $s0	# El mayor valor para construir la torre
+ add $s1, $zero, $s0	# El mayor valor para construir la torre
  
- la $t1, torreA		# Carga dirección inicial de la torre A
+ addi $t0, $zero, 0	# $t0 = 0, para BORRAR en RAM
+ addi $t1, $zero, 0x1001	# Carga parte Alta de dir. de Torre A
+ sll $t1, $t1, 16		# $t1 = 0x1001 0000
  
 Construir_torre_en_RAM:
- sw $t0, 0($t1)
- sub $t0, $t0, 1
+ sw $s1, 0($t1)		# Discos en TORRE
+ sub $s1, $s1, 1	# Restas uno para poner el disco de arriba
  addi $t1,$t1, 4	# Desplazamos la dirreccion para nuevo dato
- bne $t0, $zero, Construir_torre_en_RAM
+ bne $s1, $zero, Construir_torre_en_RAM
+
+ addi $t1, $zero, 0x1001	# la $t1, torreA	### torreA es mi  ORG
+ sll $t1, $t1, 16		# $t1 = 0x1001 0000
+ addi $t1, $t1,   0x0000	#
  
+ addi $t2, $zero, 0x1001	# la $t2, torreB	### torreB es mi  temp
+ sll $t2, $t2, 16		# $t2 = 0x1001 0020
+ addi $t2, $t2,   0x0020	#					
+
+ addi $t3, $zero, 0x1001	# la $t3, torreC	### torreC es mi  DST
+ sll $t3, $t3, 16		# $t3 = 0x1001 0040
+ addi $t3, $t3,   0x0040	#
  
  jal f_Hanoi
  
-  j EXIT
+ j EXIT
 
 ############### FUNCION RECURSIVA ###############
- f_Hanoi:
+ f_Hanoi:	# MUEVE DEL ORIGEN AL DST "SEGÚN LOS PARÁMETROS MANDADOS"
+#---------- SAVE 2 variables en STACK ----------#
+	addi $sp, $sp, -20	# Pedimos bytes de espacio del Stack
+	 sw $a0, 0($sp)		# altura: num. de discos
+	 sw $t1, 4($sp)		# dir. de ORG 
+	 sw $t2, 8($sp)		# dir. de Temp
+	 sw $t3, 12($sp)	# dir. de DST
+	 sw $ra, 16($sp)	# Address RETURN
+#-----------------------------------------------#
+ beq $a0, 1, casoBase		# if( n == 1 )
+  casoRecursivo:		# else haz el caso comun (recursivo)		
+   sub $a0, $a0, 1	# discos n-1 
+   addi $t1, $t1, 4	# posicionarme en 1 disco más arriba
+   la $t5, 0($t2)	### $t5 es AUX. para el SWAPEO
+   la $t2, 0($t3)	### Intercambiamos temp y dst
+   la $t3, 0($t5)	### dst = $t2  y  temp = $t3
  
+ jal f_Hanoi	# llamado recursivo
+ # aqui regresa el primer RETURN 
+ 
+   sub $a0, $a0, 1	# discos n-1 
+   la $t5, 0($t2) 	### $t5 es AUX. para el SWAPEO
+   la $t2, 0($t1)	### Intercamnbiamos org y temp
+   la $t1, 0($t5)	### temp = $t1  y  org = $t2
+ jal f_Hanoi	# llamado recursivo
+ 
+  casoBase:
+   lw $t4, 0($t1)		# if (n == 1) entonces: Mueve disco...
+   sw $t0, 0($t1)		# Borra disco movido
+   sw $t4, 0($t3)		# ... de A  a C
 
-
-
-
+  end_if:
+   add $v0, $zero, $t1		# RETURN con $v0
+#---------- LOAD 2 variables del STACK ----------# 
+	 lw $a0, 0($sp)
+	 lw $t1, 4($sp)
+	 lw $t2, 8($sp)
+	 lw $t3, 12($sp)
+	 lw $ra, 16($sp)
+	addi $sp, $sp, 20	
+#------------------------------------------------#
  jr $ra			# RETURN a la función recursiva
 
 ################# FIN DE PROGRAMA #################
